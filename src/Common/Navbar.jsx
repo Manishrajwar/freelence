20699ce15eca948aff8c5214526541fa80765435
Manishrajwar <manishsinghrajwar80@gmail.com>
2024-08-21 12:52:52 +0530

    done the navbar sidebar

--- src/Common/Navbar.jsx
+++ src/Common/Navbar.jsx
@@ -1,18 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import "./common.css";
 import logo from "../assets/logo.png";
 import smallnav from "../assets/smallnav.png"
+import cross22 from "../assets/cross22.png"
+import { motion } from "framer-motion";
 
 const data = ["Places", "About", "Blog", "Contact", ""];
 
 function Navbar() {
+
+  const [opensidebar, setOpensidebar] = useState(false);
+
   return (
+
+    <>
+   
     <nav className="navbarwrapper">
 
       <div className="navcont">
 
         {/* left logo  */}
-        <img src={logo} alt="" />
+        <img src={logo} alt="" className="logo" />
 
         {/* nav items */}
         <ul className="navitems">
@@ -27,10 +35,33 @@ function Navbar() {
           )}
         </ul>
 
-          <img src={smallnav} loading="lazy" alt="smallnav" className="smallnav" />
+          <img onClick={()=>setOpensidebar(true)} src={smallnav} loading="lazy" alt="smallnav" className="smallnav" />
 
       </div>
+
+
     </nav>
+
+    <motion.div
+            className={`slidebarslidecont ${opensidebar ? 'open' : ''}`}
+            initial={{ x: '100%' }} // Initial position off-screen
+            animate={{ x: opensidebar ? 0 : '100%' }} // Slide in/out based on state
+            transition={{ duration: 0.3 }} // Smooth transition
+        >
+            <div className="cross22">
+                <img onClick={() => setOpensidebar(false)} src={cross22} alt="Close" />
+            </div>
+            <div className="allnavitems">
+                {data.map((item, index) => (
+                    <p key={index} className="sinnav">
+                        {item}
+                    </p>
+                ))}
+            </div>
+        </motion.div>
+
+    </>
+
   );
 }
 
